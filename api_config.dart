e8020f282f9c4e7635f1635730b26542233fbf57
WookieBandit <48366815+WookieBandit@users.noreply.github.com>
2025-05-20 18:03:08 -0400

    api_config.dart

--- api_config.dart
+++ api_config.dart
@@ -5,12 +5,12 @@
 // See Phase 8 of the development plan for production security considerations.
 
 // For Google AI (Content Generation)
-const String googleAiApiKey = 'AIzaSyAwgo1wquqKL46uegs_V_9_9iv74bReguI'; // Replace with your actual API Key
-const String googleAiApiEndpoint = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent'; // Replace with your actual API Endpoint URL
+const String googleAiApiKey = '**********************'; // Replace with your actual API Key
+const String googleAiApiEndpoint = '**********************'; // Replace with your actual API Endpoint URL
 
 // --- For ElevenLabs Text-to-Speech (Audio Playback) ---
-const String elevenLabsApiKey = 'sk_aa9587db0279d25347c8096a776c02a037311ca4accb2ec6'; // Replace with your new, secure key
-const String elevenLabsApiBaseUrl = 'https://api.elevenlabs.io/v1'; 
+const String elevenLabsApiKey = '**********************'; // Replace with your new, secure key
+const String elevenLabsApiBaseUrl = '**********************'; 
 
 // For Google Cloud Text-to-Speech (Audio Playback)
 //const String googleCloudTtsApiKeyAndroid = 'AIzaSyDnwVWwQpdUfkJzPYgZ4guZtjYBcL6ZQQ4';
